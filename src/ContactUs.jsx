d303b72afbb899ad5d1253fda12d233532ead7be
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-16 11:44:26 -0400

    more styling, finalizing

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -1,6 +1,9 @@
 import { Link } from "react-router-dom";
 
 const ContactUs = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
     return (
         <div className='contact'>
             <h1>Contact Us</h1>
@@ -8,7 +11,7 @@ const ContactUs = () => {
             <p>Give us a call: (XXX) XXX-XXXX</p>
             <h2>or Get in touch by filling out this form!</h2>
             <div id='contactFormContainer'>
-                <form id='contactForm' onSubmit={event => event.preventDefault}>
+                <form id='contactForm' onSubmit={handleSubmit}>
                     <label>Enter Name:</label>
                     <input type='text' id='uname' name='uname' placeholder="Name" />
                     <label>Enter Email Address:</label>
